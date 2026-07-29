e54efdd60a664d257c9885b1059b6c470d9b9803
alfonsín <156380060+alfonsodeanda@users.noreply.github.com>
2026-07-29 00:18:11 -0600

    config and navbar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#EEF1FB] text-slate-900">
+      <Navbar />
+    </div>
+  );
+}
